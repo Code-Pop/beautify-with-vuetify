e9b1c9007ec0c5c77e7efb1229fe9a2d4c69fc24
Ben Hong <bencodezen@gmail.com>
2019-07-30 01:01:35 -0400

    :sparkles: Add Company Footer

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,23 @@
         </v-card-actions>
       </v-card>
     </v-content>
+    <v-footer color="primary lighten-1" padless>
+			<v-layout justify-center wrap>
+				<v-btn
+					v-for="link in links"
+					:key="link"
+					color="white"
+					text
+					rounded
+					class="my-2"
+				>
+					{{ link }}
+				</v-btn>
+				<v-flex primary lighten-2 py-4 text-center white--text xs12>
+					{{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+				</v-flex>
+			</v-layout>
+		</v-footer>
   </v-app>
 </template>
 
