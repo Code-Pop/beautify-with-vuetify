8ff2e40e058c636cdfb83cf2890085735d77c29c
Ben Hong <bencodezen@gmail.com>
2019-07-30 01:01:02 -0400

    :bug: Fix overlapping issue with Login module

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,35 @@
       <v-btn>Home</v-btn>
       <v-btn>Login</v-btn>
     </v-app-bar>
-    <!-- Login Module -->
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title class="pb-0">
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field 
-            label="Username" 
-            prepend-icon="account_circle"
-          />
-          <v-text-field 
-            :type="showPassword ? 'text' : 'password'" 
-            label="Password"
-            prepend-icon="lock"
-            :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-            @click:append="showPassword = !showPassword"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-content>
+      <!-- Login Module -->
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title class="pb-0">
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field 
+              label="Username" 
+              prepend-icon="account_circle"
+            />
+            <v-text-field 
+              :type="showPassword ? 'text' : 'password'" 
+              label="Password"
+              prepend-icon="lock"
+              :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+              @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
   </v-app>
 </template>
 
