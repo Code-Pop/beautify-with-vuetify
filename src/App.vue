de5ea06c0e0296d51c98c7d7d16e973b52838e96
Ben Hong <bencodezen@gmail.com>
2019-08-13 08:23:15 -0400

    :sparkles: Add route and view for Login page

--- src/App.vue
+++ src/App.vue
@@ -7,33 +7,7 @@
       <v-btn text rounded>Login</v-btn>
     </v-app-bar>
     <v-content>
-      <!-- Login Module -->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title class="pb-0">
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field 
-              label="Username" 
-              prepend-icon="mdi-account-circle"
-            />
-            <v-text-field 
-              :type="showPassword ? 'text' : 'password'" 
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer color="primary lighten-1" padless>
 			<v-layout justify-center wrap>
@@ -60,7 +34,6 @@ export default {
   name: 'App',
   data () {
     return {
-      showPassword: false,
       links: [
         'Home',
         'Login'
