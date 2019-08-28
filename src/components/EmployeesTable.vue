aa1cdf83b9fb9331a619c7a2138c18aa7ce71787
Ben Hong <bencodezen@gmail.com>
2019-08-28 16:59:47 -0400

    :recycle: refactor: Simplify Dashboard template

---
+++ src/components/EmployeesTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="employeesList"
+    :items-per-page="5"
+    class="elevation-1"
+    @click:row="selectRow"
+    multi-sort="true"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'EmployeesTable',
+  props: {
+    employeesList: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    headers: [
+      { text: 'Employee ID', value: 'id' },
+      { text: 'Name', value: 'name' },
+      { text: 'Position Title', value: 'title' },
+      { text: 'Salary', value: 'salary' }
+    ]
+  }),
+  methods: {
+    selectRow(event) {
+      const employee = {
+        name: event.name,
+        title: event.title
+      }
+
+      this.$emit('select-employee', employee)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
