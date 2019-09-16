6569df240a64af2f796953b18e488f0b1d5a3740
Ben Hong <bencodezen@gmail.com>
2019-09-16 17:44:47 -0400

    feature: Set responsive layout for SalesGraph

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,7 @@
     <h1>Dashboard</h1>
 
     <v-row>
-      <v-col v-for="sale in sales" :key="`${sale.title}`">
+      <v-col v-for="sale in sales" :key="`${sale.title}`" cols="12" md="4">
         <SalesGraph :sale="sale" />
       </v-col>
     </v-row>
