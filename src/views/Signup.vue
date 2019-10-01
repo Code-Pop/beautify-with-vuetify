2d49561284682e0a4ee112e17256e5cb9d6277b0
Ben Hong <bencodezen@gmail.com>
2019-10-01 00:56:31 -0400

    feature: Add base form and email input

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,6 +3,9 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
+        <v-form>
+          <v-text-field label="Email" type="email"></v-text-field>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
