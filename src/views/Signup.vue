052efc00041b0f869e9e7477d1e0fd040c031ed0
Ben Hong <bencodezen@gmail.com>
2019-10-22 21:10:44 -0400

    feature: Disable submit button unless form is valid

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form ref="signUpForm">
+        <v-form ref="signUpForm" v-model="formValidity">
           <v-text-field
             label="Email"
             type="email"
@@ -26,7 +26,13 @@
             v-model="agreeToTerms"
             :rules="agreeToTermsRules"
           ></v-checkbox>
-          <v-btn type="submit" color="primary" class="mr-4">Submit</v-btn>
+          <v-btn
+            type="submit"
+            color="primary"
+            class="mr-4"
+            :disabled="!formValidity"
+            >Submit</v-btn
+          >
           <v-btn color="warning" class="mr-4" @click="resetValidation"
             >Reset Validation</v-btn
           >
@@ -59,7 +65,8 @@ export default {
       value =>
         value.indexOf('.') <= value.length - 3 ||
         'Email should contain a valid domain extension.'
-    ]
+    ],
+    formValidity: false
   }),
   methods: {
     resetForm() {
