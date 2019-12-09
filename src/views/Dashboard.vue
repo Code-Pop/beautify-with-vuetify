ebe8a3bb77dc8b84271c0fa224c360a41632b382
Ben Hong <bencodezen@gmail.com>
2019-12-09 00:23:48 -0500

    feature: Add more content to extend length of Dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -29,6 +29,25 @@
       </v-col>
     </v-row>
 
+    <v-row id="below-the-fold">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
