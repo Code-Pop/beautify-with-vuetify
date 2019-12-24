2b9173263a171bebe3618220cee8e3ebaad1ab5b
Ben Hong <bencodezen@gmail.com>
2019-12-24 10:44:33 -0500

    feature: Define custom colors on light and dark theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,5 +6,15 @@ Vue.use(Vuetify)
 export default new Vuetify({
   icons: {
     iconfont: 'mdi'
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: '#41B883'
+      },
+      dark: {
+        primary: '#34495E'
+      }
+    }
   }
 })
